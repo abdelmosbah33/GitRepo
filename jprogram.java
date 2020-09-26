97409255a80099e650918a66d7033f5c57d686d7
root <abdelmosbah33@gmail.com>
2020-09-26 18:34:51 +0000

    this is my fourth commit

--- jprogram.java
+++ jprogram.java
@@ -1,5 +1,5 @@
 public class jprogram{
 	public static void main (String [] args){
-		System.out.println("I'm taking CS407 because i want to learn baout linux" );
+		System.out.println("my favorite food is chicken wings" );
 	}
 }
