fcb58dd013b855e3199e010878edcee391f173d0
root <abdelmosbah33@gmail.com>
2020-09-26 18:23:05 +0000

    this is my second commit

--- jprogram.java
+++ jprogram.java
@@ -1,5 +1,5 @@
 public class jprogram{
 	public static void main (String [] args){
-		System.out.println("My name  is Abdel Mosbah" );
+		System.out.println("My favorite programming language is java " );
 	}
 }
